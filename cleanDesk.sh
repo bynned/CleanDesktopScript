87c453d183e37b04edb38983465e4fae005ea59f
bynned <benny.cascarino@tuni.fi>
2023-06-21 12:58:50 +0300

    Edit

--- cleanDesk.sh
+++ cleanDesk.sh
@@ -3,11 +3,13 @@
 set -e #To stop the script if there is a problem
 echo "Beginning to move files!"
 
+junk_folder="Kaikkee2" #Change this to your junk folder name
+
 desktop_path="$HOME/Desktop"
-junk_folder="$desktop_path/Kaikkee2"
+junk_folder_path="$desktop_path/$junk_folder"
 
 shopt -s extglob
-mv "$desktop_path"/!(Kaikkee2) "$junk_folder" #Move files and directories
+mv "$desktop_path"/!(Kaikkee2) "$junk_folder_path" #Move files and directories
 shopt -u extglob
 mv "$desktop_path"/*.lnk "$kaikkee_folder" #Move shortcuts, .lnk is the file extention for shortcuts used in windows
 
